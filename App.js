db56402b3df6824a3ad19a550b8d010563617999
Chathu Abeywickrama <chathu.git93@gmail.com>
2019-07-30 14:59:49 +0530

    changes

--- App.js
+++ App.js
@@ -1,12 +1,110 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, Platform, StatusBar, AsyncStorage, ActivityIndicator, Alert, DeviceEventEmitter } from 'react-native';
+import { NavigationActions, StackActions } from 'react-navigation';
+import { Container, View, Drawer, Root } from "native-base";
+import { useScreens } from 'react-native-screens';
+
+import theme from "./src/app/theme";
+import { NavContainer } from "./src/app/loggedIn";
+import { NavLoggedOutCoutainer } from "./src/app/loggedOut";
+import { AppProvider } from "./src/app/app";
 
 import styles from './app-styles'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+useScreens();
+
+class App extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      currentToken: null,
+      currentUser: null,
+    };
+
+  }
+
+  navigate(routeName, params) {
+
+    const { nav } = this;
+
+    if (nav) {
+
+      this.hideSidebar();
+
+      setTimeout(() => {
+
+        nav.dispatch(
+          NavigationActions.navigate({ routeName, params })
+        )
+
+      }, 300);
+
+    }
+
+  }
+
+  resetNavigation(routes) {
+
+    const { nav } = this;
+
+    if (nav) {
+
+      const resetAction = StackActions.reset({
+        index: 0,
+        actions: routes || [NavigationActions.navigate({ routeName: 'news' })],
+      });
+
+      nav.dispatch(resetAction);
+
+    }
+
+  }
+
+  render() {
+
+    const { mounted, sidebarOpen, currentToken, currentUser } = this.state;
+
+    const persistenceKey = "NavigationStateDEV4" + currentToken;
+
+    const persistNavigationState = !__DEV__ ? undefined : async (navState) => {
+      try {
+        await AsyncStorage.setItem(persistenceKey, JSON.stringify(navState))
+      } catch (err) {
+        // handle the error according to your needs
+      }
+    };
+
+    const loadNavigationState = !__DEV__ ? undefined : async () => {
+      const jsonString = await AsyncStorage.getItem(persistenceKey)
+      return JSON.parse(jsonString)
+    }
+
+    return (
+      <Container styles={styles.wrap}>
+
+        <StatusBar backgroundColor={"#f8509a"} barStyle="light-content" translucent animated />
+
+        <Root>
+
+          <AppProvider>
+
+            <NavLoggedOutCoutainer
+              persistNavigationState={persistNavigationState}
+              loadNavigationState={loadNavigationState}
+              ref={el => { this.nav = el }}
+            />
+
+          </AppProvider>
+
+        </Root>
+
+      </Container>
+    );
+  }
 }
+
+export default App;
+
--- app-styles.js
+++ app-styles.js
@@ -1,6 +1,15 @@
 import { StyleSheet } from 'react-native'
+import { Platform, StatusBar } from 'react-native'
 
 export default StyleSheet.create({
+  wrap: {
+    flex: 1,
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight
+      }
+    })
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
